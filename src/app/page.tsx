b97143248a369ee880b29cfc499630b5cf387e17
formajesty <mbiruk118@gmail.com>
2025-09-20 11:16:06 +0300

    third

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,11 @@ import { ScrollTrigger, SplitText } from "gsap/all";
 import { gsap } from "gsap";
 import Navbar from "~/components/Navbar";
 import Hero from "~/components/Hero";
+import Cocktails from "~/components/Cocktails";
+import About from "~/components/About";
+import Art from "~/components/Art";
+import Menu from "~/components/Menu";
+import Contact from "~/components/Contact";
 
 gsap.registerPlugin(ScrollTrigger, SplitText);
 
@@ -13,6 +18,11 @@ export default async function HomePage() {
    <main className="h-full w-full bg-black text-white">
     <Navbar />
     <Hero/>
+    <Cocktails/>
+    <About/>
+     <Art />
+	 <Menu />
+	 <Contact />
    </main>
   );
 }
