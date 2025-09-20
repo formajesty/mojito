2510a0efdd9b128e43bc6f1e9dd0a7475ebae049
formajesty <mbiruk118@gmail.com>
2025-09-20 10:42:04 +0300

    second

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,8 @@
 import Link from "next/link";
 import { ScrollTrigger, SplitText } from "gsap/all";
 import { gsap } from "gsap";
+import Navbar from "~/components/Navbar";
+import Hero from "~/components/Hero";
 
 gsap.registerPlugin(ScrollTrigger, SplitText);
 
@@ -8,11 +10,9 @@ export default async function HomePage() {
     
   
   return (
-    <div className="flex-center">
-      <h1 className="text-3xl font-bold underline">
-        Hello, GSAP
-      </h1>
-      
-    </div>
+   <main className="h-full w-full bg-black text-white">
+    <Navbar />
+    <Hero/>
+   </main>
   );
 }
